811704533611c6effc4b1ff1ce1a60c8bb4b1da4
SURESH-S-U <suresh.elite07@gmail.com>
2025-03-05 15:04:02 +0530

    MongoDB connected

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.listen(3000, () => {
@@ -6,5 +7,14 @@ app.listen(3000, () => {
 });
 
 app.get('/',(req,res) => {
-    res.send("The message is from API");
+    res.send("The message is from API updated");
 });
+
+mongoose.connect('mongodb+srv://admin:SureshDatabase@cluster0.g6utv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => {
+    console.log("Database Connected");
+})
+.catch(() => {
+    console.log("Database not connected");
+})
+
