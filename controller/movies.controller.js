aa4139c6aefaef09c6821e449fd120f219435e51
SURESH-S-U <suresh.elite07@gmail.com>
2025-03-05 22:22:44 +0530

    Controllers Added

---
+++ controller/movies.controller.js
@@ -0,0 +1,16 @@
+
+export const MovieGet = (req, res) => {
+    res.send("Get all movie list.");
+}
+
+export const MovieCreate = (req, res) => {
+    res.send("Create movies.");
+}
+
+export const MovieUpdate = (req, res) => {
+    res.send("Movies updated.");
+}
+
+export const MovieDelete = (req, res) => {
+    res.send("Movies deleted.");
+}
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,27 +1,20 @@
 import express from 'express';
+import { MovieCreate, MovieDelete, MovieGet, MovieUpdate } from '../controller/movies.controller';
 
 const router = express.Router();
 
 // CRUD functionalities of Movies
 
 // R - for reading
-router.get('/', (req, res) => {
-    res.send("Get all movie list.");
-});
+router.get('/', MovieGet);
 
 // C - for creating a movie
-router.post('/', (req, res) => {
-    res.send("Create movies.");
-});
+router.post('/', MovieCreate);
 
 // U - for updating the movie
-router.put('/:id', (req, res) => {
-    res.send("Movies updated.");
-});
+router.put('/:id', MovieUpdate);
 
 // D - for deleting the movie
-router.delete('/:id', (req, res) => {
-    res.send("Movies deleted.");
-});
+router.delete('/:id', MovieDelete);
 
 export default router;
